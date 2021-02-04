941c554ed0a6d503ad3492624e050ad1b4cc1b5c
Ben <76835568+Ben84120@users.noreply.github.com>
2021-02-04 14:43:16 +0100

    MAJ

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -13,32 +13,11 @@ public class AnalyticsCounter {
 		 * Using the ISymptomReader interface to read the symptoms.txt file and place its contents in an alphabetically arranged collection.
 		 */
 		AnalyticsCounter counter=new AnalyticsCounter();
-		List<String> symptoms= counter.readingFile("Project02Eclipse/symptoms.txt");
-		
+		List<String>symptoms= counter.readingFile("Project02Eclipse/symptoms.txt");
 		Collections.sort(symptoms);
 		System.out.println(symptoms.toString());
-
-		Map<String, Integer> map = new HashMap<>();
-		/**
-		 * scan the MAP to count the occurrences of each symptom.
-		 */
-		for (String symptom : symptoms) {
-
-			if (symptom != null) {
-
-				if (!map.containsKey(symptom)) {
-					map.put(symptom, 1);
-				} else {
-					map.put(symptom, map.get(symptom) + 1);
-				}
-			}
-
-		}
 		
-
-		/**
-		 * Use the interface IWriterMap to write the data on a new file
-		 */
+		
 		IWriterMap writer = new WriteFromDataMap();
 		writer.ecrireResult(map);
 
@@ -53,5 +32,21 @@ public class AnalyticsCounter {
 	
 	
 	
+	private Map<String, Integer> map = new HashMap<>();{
 	
+		for (String symptom : symptoms) {
+
+			if (symptom != null) {
+
+			if (!map.containsKey(symptom)) {
+				map.put(symptom, 1);
+			} else {
+				map.put(symptom, map.get(symptom) + 1);
+			}
+		}
+
+	}
+	
+	
+}
 }
