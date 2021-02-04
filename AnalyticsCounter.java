b466d727594acdaea7336de295d109dded7e2029
Salah eddine Ammour <seammour@gmail.com>
2021-02-04 15:50:46 +0100

    dernier commit

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -6,47 +6,41 @@ import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
+public static void main(String args[]) throws Exception {
 
-	public static void main(String args[]) throws Exception {
-		
-		/**
-		 * Using the ISymptomReader interface to read the symptoms.txt file and place its contents in an alphabetically arranged collection.
-		 */
-		AnalyticsCounter counter=new AnalyticsCounter();
-		List<String>symptoms= counter.readingFile("Project02Eclipse/symptoms.txt");
-		Collections.sort(symptoms);
-		System.out.println(symptoms.toString());
-		Map<String, Integer> map = cumputingSymptoms(symptoms)
-		
-		IWriterMap writer = new WriteFromDataMap();
-		writer.ecrireResult(map);
+        /**
+         * Using the ISymptomReader interface to read the symptoms.txt file and place its contents in an alphabetically arranged collection.
+         */
+        AnalyticsCounter counter=new AnalyticsCounter();
+        List<String>symptoms= counter.readingFile("Project02Eclipse/symptoms.txt");
+        Collections.sort(symptoms);
+        System.out.println(symptoms.toString());
+        Map<String, Integer> map = counter.cumputingSymptoms( symptoms);
 
-	}
-	
-	private List<String> readingFile(String file) {
-		ISymptomReader reader = new ReadSymptomDataFromFile(file);
-		return reader.GetSymptoms();
-		
-	}
-	
-	
-	private Map<String, Integer> cumputingSymptoms(List<String> symptoms) {
-		Map<String, Integer> map = new HashMap<>();
-		for (String symptom : symptoms) {
+        IWriterMap writer = new WriteFromDataMap();
+        writer.ecrireResult(map);
 
-			if (symptom != null) {
+    }
 
-			if (!map.containsKey(symptom)) {
-				map.put(symptom, 1);
-			} else {
-				map.put(symptom, map.get(symptom) + 1);
-			}
-		}
-		return map;	
-	}
-	
-	
-	
-	
-}
+    private List<String> readingFile(String file) {
+        ISymptomReader reader = new ReadSymptomDataFromFile(file);
+        return reader.getSymptoms();
+
+    }
+
+
+    private Map<String, Integer> cumputingSymptoms(List<String> symptoms) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String symptom : symptoms) {
+            if (symptom != null) {
+
+                if (!map.containsKey(symptom)) {
+                    map.put(symptom, 1);
+                } else {
+                    map.put(symptom, map.get(symptom) + 1);
+                }
+            }
+        }
+        return map;
+    }
 }
